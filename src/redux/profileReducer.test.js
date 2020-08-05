82b117559cbc6f577fb9ea2184897bfef609e8e5
VladilavDenis <32334309vlad@gmail.com>
2020-08-05 19:04:34 +0300

    unit tests for reducer profile
    Signed-off-by: VladilavDenis <32334309vlad@gmail.com>

--- src/redux/profileReducer.test.js
+++ src/redux/profileReducer.test.js
@@ -1,19 +1,18 @@
 import profileReducer, {actionCreator} from "./profileReducer";
-import React from "react"
 
 let startState = {
     posts: [
         {id: 1, message: "This my first post", likeCounts: '26'},
         {id: 2, message: "This my second post", likeCounts: '27'}
     ],
-}
+};
 
 test('new post should be added', () => {
     //1. test data
-    let action = actionCreator.addPost("89 level")
+    let action = actionCreator.addPost("89 level");
 
     // 2. action
-    let newState = profileReducer(startState, action)
+    let newState = profileReducer(startState, action);
 
     // 3. expectation
     expect(newState.posts.length).toBe(3)
@@ -22,10 +21,10 @@ test('new post should be added', () => {
 test('message of new post should be correct', () => {
     //1. test data
 
-    let action = actionCreator.addPost("89 level")
+    let action = actionCreator.addPost("89 level");
 
     // 2. action
-    let newState = profileReducer(startState, action)
+    let newState = profileReducer(startState, action);
 
     // 3. expectation
     expect(newState.posts[2].message).toBe("89 level")
@@ -36,10 +35,10 @@ test('message of new post should be correct', () => {
 test('delete post should be correct', () => {
     //1. test data
 
-    let action = actionCreator.deletePost(1)
+    let action = actionCreator.deletePost(1);
 
     // 2. action
-    let newState = profileReducer(startState, action)
+    let newState = profileReducer(startState, action);
 
     // 3. expectation
     expect(newState.posts.length).toBe(1)
@@ -48,10 +47,10 @@ test('delete post should be correct', () => {
 test(`after deleting length shouldn't be decrement if id is incorrect`, () => {
     //1. test data
 
-    let action = actionCreator.deletePost(2005880)
+    let action = actionCreator.deletePost(2005880);
 
     // 2. action
-    let newState = profileReducer(startState, action)
+    let newState = profileReducer(startState, action);
 
     // 3. expectation
     expect(newState.posts.length).toBe(2)
