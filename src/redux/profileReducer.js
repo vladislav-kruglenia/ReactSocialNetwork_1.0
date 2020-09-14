6ba4f0d97b1ff39af8c3e5fe05b3cefe51c9c9dc
vladislav-kruglenia <32334309vlad@gmail.com>
2020-09-14 18:51:32 +0300

    try-catch, router switch, classnames
    Signed-off-by: vladislav-kruglenia <32334309vlad@gmail.com>

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -118,9 +118,12 @@ export let getStatusThunkCreator = (numberID) => {
 
 export let updateStatusThunkCreator = (status) => {
     return async (dispatch) => {
-        let response = await profileAPI.updateStatus(status)
+        try {let response = await profileAPI.updateStatus(status)
         if (response.data.resultCode === 0) {
             dispatch(setStatus(status))
+        }}
+        catch{
+            //
         }
     }
 }
